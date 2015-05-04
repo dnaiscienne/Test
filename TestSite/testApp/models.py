70917ebf00ba30b9658e9b1ce3abb40be1460340
dc <dc@as>
2015-05-04 16:32:25 +0800

    updated test models

--- TestSite/testApp/models.py
+++ TestSite/testApp/models.py
@@ -7,5 +7,16 @@ class Topic(models.Model):
     topic_description = models.CharField(max_length=200)
 
 class Question(models.Model):
+    QUESTION_DIFFICULTY_LEVELS = (
+        (EASY, 'Easy'),
+        (MODERATE, 'Moderate'),
+        (HARD, 'Hard'),
+    )
     question_type = models.CharField(max_length=20)
-    question_text = models.CharField(max__length=500)
+    question_text = models.CharField(max_length=500)
+    question_diffficulty = models.CharField(max_length=15, choices=QUESTION_DIFFUCULTY_LEVELS, default=EASY)
+
+class Choice(models.Model):
+    choice_is_correct = models.BooleanField(default=False)
+    choice_text = models.CharField(max_length=200)
+    
