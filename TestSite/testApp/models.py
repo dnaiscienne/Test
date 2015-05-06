797f5a7dfc4e423c372aeb21d094d2ae37b4efb4
dc <dc@as>
2015-05-06 16:32:16 +0800

    Modified testApp models. Added relationships.

--- TestSite/testApp/models.py
+++ TestSite/testApp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class OnlineTest(models.Model):
+    exams = model.ManyToManyField(Publication)
     online_test_name = models.CharField(max_length=50)
     online_test_description = models.CharField(max_length=200)
     
@@ -12,16 +13,19 @@ class Question(models.Model):
         (MODERATE, 'Moderate'),
         (HARD, 'Hard'),
     )
+    online_test = models.ForeignKey(OnlineTest)
     question_type = models.CharField(max_length=20)
     question_text = models.CharField(max_length=500)
     question_diffficulty = models.CharField(max_length=15, choices=QUESTION_DIFFUCULTY_LEVELS, default=EASY)
 
 class Choice(models.Model):
+    question = models.ForeignKey(Question)
     choice_is_correct = models.BooleanField(default=False)
     choice_text = models.CharField(max_length=200)
     
 class Exam(models.Model):
-    exam_date= models.DateField('date of exam')
+    examinee = models.ForeignKey(Examinee)
+    exam_date = models.DateField('date of exam')
     exam_time_started = models.TimeField('start time')
     exam_time_ended = models.TimeField()
 
